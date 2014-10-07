f77e0d75ba9bd75963b81e96b02c651182e65fb6
mathieubenoit <mathieu.benoit@cern.ch>
2014-10-07 13:30:04 +0200

    initial commit

---
+++ JudithData.py
@@ -0,0 +1,114 @@
+from ROOT import * 
+
+
+
+class JudithData:
+    """A container for TBTrack Data """
+
+    RunNumber = 0
+
+    judith_file = 0
+
+    planeTrees = []
+    
+
+    HitInCluster = []
+    NHits = []
+    PixX = []
+    PixY = []
+    Lv1 = []
+    ToT = []
+    
+    
+    
+    def __init__(self,rootfile_name,nplanes) :
+        
+        self.judith_file = TFile(rootfile_name,"open")
+ 
+        for plane in range(nplanes):       
+            self.HitInCluster.append(0)
+            self.NHits.append(0)
+            self.PixX.append(0)
+            self.PixY.append(0)
+            self.Lv1.append(0)
+            self.ToT .append(0)   
+        
+        for plane in range(nplanes):            
+            self.planeTrees.append(self.judith_file.Get("Plane%i/Hits"%plane))
+
+  
+        for plane in range(nplanes):            
+            self.HitInCluster[plane]   = self.planeTrees[plane].HitInCluster
+            self.NHits[plane]           = self.planeTrees[plane].NHits
+            self.PixX[plane]            = self.planeTrees[plane].PixX
+            self.PixY[plane]            = self.planeTrees[plane].PixY
+            self.Lv1[plane]             = self.planeTrees[plane].Timing
+            self.ToT [plane]            = self.planeTrees[plane].Value
+                    
+    
+    def GetEvent(self,event_nr):
+        
+        for tree in  self.planeTrees : 
+            tree.GetEntry(event_nr)
+        
+        
+        telescopeData = [[] for i in range(len(self.planeTrees))]
+        
+        for i in range(len(self.planeTrees)) :
+            for j,tot in enumerate(self.ToT) : 
+                
+                if((self.PixX[i][j]!=0 and self.PixY[i][j]!=0) and (self.ToT[i][j]!=0 and self.Lv1[i][j]!=0)) :
+                    if((self.PixX[i][j]>=0 and self.PixY[i][j]>=0) and (self.PixX[i][j]<80 and self.PixY[i][j]<=336)) :
+                        telescopeData[i].append([self.PixX[i][j],self.PixY[i][j],self.ToT[i][j],self.Lv1[i][j]])
+            
+            
+        return telescopeData
+        
+
+        
+    def GetNEvents(self):
+        return self.planeTrees[0].GetEntries()
+        
+    
+    
+    def PrintEvent(self,eventnr):
+        print "-----------------------------------------"
+        telescopeData=self.GetEvent(eventnr)
+        
+        print "Event #%i"%eventnr
+        for i in range(len(telescopeData)):
+            
+            print "Plane %i"%i
+            
+            for hit in telescopeData[i] : 
+                print "X:%i Y:%i TOT:%i LV1:%i"%(hit[0],hit[1],hit[2],hit[3])
+            
+
+
+# aDataSet = JudithData("/data/Testbeam_Data/August2014_HVCMOS_FEI4/RunData/cosmic_C19_2207-2222.root",7)
+# 
+# 
+# for i in range(1000) : 
+# 
+#     aDataSet.PrintEvent(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TelescopeConverter.py
@@ -0,0 +1,183 @@
+'''
+Created on June 17, 2013
+
+Tool to convert allpix telescope simulation files into LCIO EUTelescope format.
+
+@author: <a href="mailto:samir.arfaoui@cern.ch">Samir Arfaoui</a>
+'''
+
+from pyLCIO import  UTIL
+from ROOT import TVector3, TLorentzVector, TRandom3, TMath, std, TH1D, TCanvas
+from JudithData  import *
+
+
+#Compiled with Cython if available, comment otherwise
+#import pyximport; pyximport.install(pyimport=True)
+
+from pyLCIO import EVENT,IMPL,IOIMPL
+from time import time
+import sys, math, glob, tarfile
+from collections import defaultdict
+
+telescopeTestDict = {}
+telescopeTestDict['300'] = 0
+telescopeTestDict['301'] = 0
+telescopeTestDict['302'] = 0
+telescopeTestDict['303'] = 0
+telescopeTestDict['304'] = 0
+telescopeTestDict['305'] = 0
+
+print sys.argv[3:]
+
+mySensorIDlist = sys.argv[3:]
+for mySensorID in mySensorIDlist:
+    telescopeTestDict[mySensorID] = 0
+
+
+
+
+
+#########################################################################################
+def convertRun( inputFileName, outputFileName, runNumber, nplanes=7 ):
+    
+
+    # define detector name
+    detectorName = 'FEI4Tel'
+
+    # create a writer and open the output file
+    writer = IOIMPL.LCFactory.getInstance().createLCWriter()
+    writer.open( outputFileName, EVENT.LCIO.WRITE_NEW )
+
+    # create a run header and add it to the file
+    run = IMPL.LCRunHeaderImpl()
+    run.setRunNumber( runNumber )
+    run.setDetectorName( detectorName )
+    run.parameters().setValue  ( 'GeoID'            , 0 )
+    run.parameters().setValues ( 'MaxX'             , std.vector(int)(6,335) ) 
+    run.parameters().setValues ( 'MaxY'             , std.vector(int)(6,79) )
+    run.parameters().setValues ( 'MinX'             , std.vector(int)(6,0) ) 
+    run.parameters().setValues ( 'MinY'             , std.vector(int)(6,0) )
+    run.parameters().setValue  ( 'NoOfDetector'     , 6 )
+    run.parameters().setValues ( 'AppliedProcessor' , std.vector('string')(1,'') )
+    run.parameters().setValue  ( 'DAQHWName'        , 'EUDRB' )
+    run.parameters().setValue  ( 'DAQSWName'        , 'EUDAQ' )
+    run.parameters().setValue  ( 'DataType'         , 'Data' )
+    run.parameters().setValue  ( 'DateTime'         , '24.12.2000  23:59:59.000000000' )
+    run.parameters().setValue  ( 'EUDRBDet'         , 'MIMOSA26' )
+    run.parameters().setValue  ( 'EUDRBMode'        , 'ZS2' )
+    writer.writeRunHeader( run )
+  
+
+    aDataSet = JudithData("/data/Testbeam_Data/August2014_HVCMOS_FEI4/RunData/cosmic_C19_2207-2222.root",7)
+
+    MAXEVENTS = aDataSet.GetNEvents()
+    #MAXEVENTS = 200000
+ 
+    NEVENTS   = 0
+
+    # event loop
+    for eventnr in range(MAXEVENTS):
+
+
+        if ( eventnr%1000 == 0 ):
+            print 'Events processed: %i ...' % eventnr
+
+        # create an event and set its parameters
+        event = IMPL.LCEventImpl()
+        event.setEventNumber( eventnr )
+        event.setDetectorName( detectorName )
+        event.setRunNumber( runNumber )
+        event.setTimeStamp( int( time() * 1000000000. ) )
+        event.parameters().setValue( 'EventType', 2 )
+
+        # parse input file
+        telescopeData=aDataSet.GetEvent(eventnr)
+
+
+        # if first event, create additional setup collection(s)
+        if eventnr == 0:
+            
+            eudrbSetup = IMPL.LCCollectionVec( EVENT.LCIO.LCGENERICOBJECT )
+            
+            # collection parameters
+            eudrbSetup.parameters().setValue( 'DataDescription', 'type:i,mode:i,spare1:i,spare2:i,spare3:i' )
+            eudrbSetup.parameters().setValue( 'TypeName', 'Setup Description' )
+            
+            # create on setup object per Telescope plane
+            for sensorID in range(nplanes):
+                                    
+                setupObj = IMPL.LCGenericObjectImpl(5,0,0)
+                setupObj.setIntVal( 0, 102 )
+                setupObj.setIntVal( 1, 101 )     
+                eudrbSetup.addElement( setupObj )
+
+            event.addCollection ( eudrbSetup, 'eudrbSetup' )
+
+
+        # ID encoder info
+        encodingString = 'sensorID:5,sparsePixelType:5'
+
+        # Telescope data collection
+        trackerDataColl = IMPL.LCCollectionVec( EVENT.LCIO.TRACKERDATA )
+        idEncoder_Telescope = UTIL.CellIDEncoder( IMPL.TrackerDataImpl )( encodingString, trackerDataColl )
+
+
+        # fill telescope collection
+        for sensorID in range(nplanes):
+            
+            planeData = IMPL.TrackerDataImpl()
+
+            idEncoder_Telescope.reset()
+            idEncoder_Telescope['sensorID'] = int( sensorID ) # cannot fit 300 in 5 bits!! FIXME
+            idEncoder_Telescope['sparsePixelType'] = 2
+            idEncoder_Telescope.setCellID( planeData )
+            
+            # loop over hits
+            chargeVec = std.vector(float)()
+            for hit in telescopeData[sensorID]:
+                for val in hit :
+                    chargeVec.push_back( val )
+
+            planeData.setChargeValues( chargeVec )
+
+            trackerDataColl.addElement( planeData )
+
+        event.addCollection( trackerDataColl, 'zsdata_FEI4' )
+
+        writer.writeEvent( event )
+    
+    writer.flush()
+    writer.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#########################################################################################
+def usage():
+    print 'Converts allpix generated telescope files into LCIO format'
+    print 'Usage:\n python %s <inputRootFile> <outputFile> runnumber' % ( sys.argv[0] )
+
+#########################################################################################
+
+
+
+
+
+if __name__ == '__main__':
+    if len( sys.argv ) < 3:
+        usage()
+        sys.exit( 1 )
+    convertRun( sys.argv[1], sys.argv[2],int(sys.argv[3]))
+
+    for sensorID in sorted( telescopeTestDict.iterkeys() ):
+        print sensorID, telescopeTestDict[sensorID]
