3b468ff7a779e4d1dfdf4f35c2e8da4551848971
mathieubenoit <mathieu.benoit@cern.ch>
2014-12-04 08:57:01 +0100

    Changed path for software and file copy

--- Convert.py
+++ Convert.py
@@ -4,8 +4,8 @@ run = int(sys.argv[1])
 
 print "converting Run %06i"%run
 
-os.system("xrdcp root://eosatlas.cern.ch//eos/atlas/atlascerngroupdisk/pixel-upgrade/itk/BeamTest/cern_2014_october_FEI4tel/cosmic_%06i/cosmic_%06i.root ."%(run,run))
+os.system("xrdcp root://eosatlas.cern.ch//eos/atlas/atlascerngroupdisk/pixel-upgrade/itk/BeamTest/cern_2014_october_FEI4tel/cosmic_%06i.root ."%(run))
 	
-os.system("python TelescopeConverter.py cosmic_%06i.root run%06i-converter.slcio %i 7"%(run,run,run))
+os.system("python /afs/cern.ch/user/m/mbenoit/workspace/Conversion/FEI4Telescope2SLCIO/TelescopeConverter.py cosmic_%06i.root run%06i-converter.slcio %i 8"%(run,run,run))
 
-os.system("xrdcp run%06i-converter.slcio root://eosatlas.cern.ch//eos/atlas/atlascerngroupdisk/pixel-upgrade/itk/BeamTest/cern_2014_october_FEI4tel/lcio/run%06i-converter.slcio"%(run,run))
+os.system("xrdcp -f run%06i-converter.slcio root://eosatlas.cern.ch//eos/atlas/atlascerngroupdisk/pixel-upgrade/itk/BeamTest/cern_2014_october_FEI4tel/lcio/run%06i-converter.slcio"%(run,run))
